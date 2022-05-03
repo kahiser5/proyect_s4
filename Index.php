79f81abdd898135bac9465dee5b623742ddfde8c
kahiser5 <102568156+kahiser5@users.noreply.github.com>
2022-05-03 18:03:15 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Proyecto pracial 2</title>
+        <link rel="stylesheet" href="Diseno.css">
+		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.0/dist/sweetalert2.all.min.js"></script>
+    </head>
+    <body>
+	<script src="Index_secundario.js"></script>
+    <script>
+		login()
+	</script>
+    </body>
+</html>
